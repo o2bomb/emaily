c398f597c63210b58036016127ee3964e8e908aa
o2bomb <felixtan92@gmail.com>
2019-12-27 01:25:15 +0800

    Add link to Google OAuth process

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Suck my big faat dick
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>Suck my big faat dick</p>
+        <a className="App-link" href="/auth/google">
+          Sign In With Google
         </a>
       </header>
     </div>
