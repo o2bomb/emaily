80dc055b358bc6b4574b4910f8e01d5170ed2d63
o2bomb <felixtan92@gmail.com>
2020-01-04 02:44:24 +0800

    Implement redux, with redux-thunk middleware

---
+++ client/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { FETCH_USER } from "./types";
+
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+  dispatch({ type: FETCH_USER, payload: res });
+};
+
+// Equivalent to the following code:
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios
+//       .get("/api/current_user")
+//       .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+// };
