348f67b233208fb3496bf2189cf432a43272e165
o2bomb <felixtan92@gmail.com>
2020-01-04 02:52:55 +0800

    Utilize Materialize CSS library

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">Emaily</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
